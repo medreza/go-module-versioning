881e594b85e0ac27af9435adb492ea9de63456b6
Ahmed Reza Rafsanzani <rza272@gmail.com>
2023-12-01 13:15:06 +0700

    add shout louder on v3

--- hello.go
+++ hello.go
@@ -12,3 +12,7 @@ type Hello struct {
 func (h *Hello) Shout() {
 	fmt.Println("HELLO, ", strings.ToUpper(h.Name))
 }
+
+func (h *Hello) ShoutLouder() {
+	fmt.Println("HELLO, ", strings.ToUpper(h.Name), "!!!")
+}
